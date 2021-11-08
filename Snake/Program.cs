a6e333c7041e34e7808e071376be15caf9a129f1
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-08 18:12:39 +0300

    Lesson-3
    create class Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,7 +6,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p1.x = 4;
+            p1.y = 5;
+            p1.sym = '#';
+            p1.Draw();
+
+            Console.ReadLine();
         }
+
     }
 }
