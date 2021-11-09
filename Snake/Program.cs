2199841be38eed5070e8516463d97e4b29e19933
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-09 15:51:53 +0300

    Lesson-4

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,17 +6,13 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
+
             p1.Draw();
 
-            Point p2 = new Point();
-            p1.x = 4;
-            p1.y = 5;
-            p1.sym = '#';
-            p1.Draw();
+            Point p2 = new Point(4, 5, '#');
+ 
+            p2.Draw();
 
             Console.ReadLine();
         }
