920b88e4284184f3aaaa649cd954319aa0cb24c9
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-09 20:40:20 +0300

    lesson-6

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Snake
 {
@@ -11,8 +13,31 @@ namespace Snake
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
+<<<<<<< Updated upstream
  
             p2.Draw();
+=======
+
+            object p = p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+>>>>>>> Stashed changes
 
             Console.ReadLine();
         }
