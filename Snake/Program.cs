630d6879433581082b88603ea38bc7852582dcfb
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-10 11:46:30 +0300

    lesson-7

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,32 +13,11 @@ namespace Snake
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-<<<<<<< Updated upstream
  
             p2.Draw();
-=======
 
-            object p = p2.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
->>>>>>> Stashed changes
-
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
             Console.ReadLine();
         }
 
