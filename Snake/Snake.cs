24bd053178bd2ecf8c969de120155e32702a28c7
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-13 14:29:43 +0300

    полиморфизм

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -6,48 +6,64 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Snake : Figure
-    {
-        Direction direction;
-        public Snake(Point tail, int length, Direction _direction)
-        {
-            direction = _direction;
-            pList = new List<Point>();
-            for (int i = 0; i < length; i++)
-            {
-                Point p = new Point(tail);
-                p.Move(i, direction);
-                pList.Add(p);
+	class Snake : Figure
+	{
+		Direction direction;
 
-            }
-        }
-        internal void Move()
-        {
-            Point tail = pList.First();
-            pList.Remove(tail);
-            Point head = GetNextPoint();
-            pList.Add(head);
+		public Snake(Point tail, int length, Direction _direction)
+		{
+			direction = _direction;
+			pList = new List<Point>();
+			for (int i = 0; i < length; i++)
+			{
+				Point p = new Point(tail);
+				p.Move(i, direction);
+				pList.Add(p);
+			}
+		}
 
-            tail.Clear();
-            head.Draw();
-        }
-        public Point GetNextPoint()
-        {
-            Point head = pList.Last();
-            Point nextPoint = new Point(head);
-            nextPoint.Move(1, direction);
-            return nextPoint;
-        }
-        public void HandleKey(ConsoleKey key)
-        {
-            if (key.Key == ConsoleKey.LeftArrow)
-                snake.direction = Direction.LEFT;
-            else if (key.Key == ConsoleKey.RightArrow)
-                snake.direction = Direction.RIGHT;
-            else if (key.Key == ConsoleKey.DownArrow)
-                snake.direction = Direction.DOWN;
-            else if (key.Key == ConsoleKey.UpArrow)
-                snake.direction = Direction.UP;
-        }
-    }
+		internal void Move()
+		{
+			Point tail = pList.First();
+			pList.Remove(tail);
+			Point head = GetNextPoint();
+			pList.Add(head);
+
+			tail.Clear();
+			head.Draw();
+		}
+
+		public Point GetNextPoint()
+		{
+			Point head = pList.Last();
+			Point nextPoint = new Point(head);
+			nextPoint.Move(1, direction);
+			return nextPoint;
+		}
+
+		public void HandleKey(ConsoleKey key)
+		{
+			if (key == ConsoleKey.LeftArrow)
+				direction = Direction.LEFT;
+			else if (key == ConsoleKey.RightArrow)
+				direction = Direction.RIGHT;
+			else if (key == ConsoleKey.DownArrow)
+				direction = Direction.DOWN;
+			else if (key == ConsoleKey.UpArrow)
+				direction = Direction.UP;
+		}
+
+		internal bool Eat(Point food)
+		{
+			Point head = GetNextPoint();
+			if (head.IsHit(food))
+			{
+				food.sym = head.sym;
+				pList.Add(food);
+				return true;
+			}
+			else
+				return false;
+		}
+	}
 }
